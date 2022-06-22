98557663bcea5ce3521c9e670867e7b8664973e7
eleache <IKPEdLzwoD26aKE@gmail.com>
2022-06-22 03:58:36 +0200

    'o'

--- traductor.py
+++ traductor.py
@@ -26,4 +26,23 @@ data = train_data
 # clean
 data.drop(["id", "date", "query", "user"],
           axis=1,
-          inplace)
+          inplace = True )
+
+def clean_tweet(tweeter):
+  tweet = BeautifulSoup(tweet, "LxmL").get_text()
+  tweet = re.sub(r"@[A-Za-z0-9]+", ' ', tweet)
+  tweet = re.sub(r"https?://[A-Za-z0-9./]+", ' ', tweet)
+  tweet = re.sub(r" +", ' ', tweet)
+  return tweet
+
+
+data_clean = [clean_tweet(tweet) for tweet in data.text]
+
+data_labels = data.sentiment.values
+data_labels[data_labels == 4] = 1
+
+tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(data_clean, target_vocab_size=2**16)
+
+data_inputs = [tokenizer.encode(sentence) for sentence in data_clean]
+
+
